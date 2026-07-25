ec37319f4cf62f3dbfaabbd0ba5015e2888ce7c4
Abhijeet <abhijeetjain4874@gmail.com>
2026-07-25 18:18:53 +0530

    Created server foundation

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
 
 dotenv.config();
@@ -13,11 +13,11 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("ImageGuard Backend is Running 🚀");
+    res.send("ImageGuard API Running...");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
